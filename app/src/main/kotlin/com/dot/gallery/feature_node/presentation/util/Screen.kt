652c6be5f55629fc35acc7f6a806c470ab422bfb
阿呆 <186315423+ADAIBLOG@users.noreply.github.com>
2026-09-21 11:51:57 +0800

    Update Screen.kt

--- app/src/main/kotlin/com/dot/gallery/feature_node/presentation/util/Screen.kt
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/util/Screen.kt
@@ -88,6 +88,7 @@ sealed class Screen(val route: String) {
     data object SettingsScreen : Screen("settings_screen")
     data object ColorPaletteScreen : Screen("color_palette_screen")
     data object SettingsGeneralScreen : Screen("settings_general_screen")
+    data object SettingsExcludedFoldersScreen : Screen("settings_excluded_folders_screen")
     data object SettingsSmartFeaturesScreen : Screen("settings_smart_features_screen")
     data object SettingsCloudIndexingScreen : Screen("settings_cloud_indexing_screen")
     data object SmartScanPreferenceDetailScreen : Screen("smart_scan_preference_detail_screen")
